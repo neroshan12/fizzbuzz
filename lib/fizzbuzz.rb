84d6f4159a76d88fe639221036401d7e2ca257ce
Neroshan Sivapalasundram <neroshan12@gmail.com>
2017-08-28 11:21:13 +0100

    Add minimal production code to meet expectations defined by new test (fizzbuzz)

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
     return "fizz"
-  else
+  else number % 5 == 0
     return "buzz"
   end
 end
